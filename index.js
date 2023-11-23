56b923682d2f4457f8c0063463293a90bc423cf7
nosirbek030381 <ismoilovn899@gmail.com>
2023-11-23 15:32:12 +0500

    Token was saved

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+import os from 'os';
 import getArgv from './helper/argv.js';
 import { printError, printHelp, printSuccess } from './service/log.service.js';
+import { saveKeyValue } from './service/storage.service.js';
+
+const saveToken = async token => {
+	try {
+		await saveKeyValue('token', token);
+		printSuccess('Saving token');
+	} catch (error) {
+		printError(error.message);
+	}
+};
+
+console.log(os.homedir());
 
 const startCLI = () => {
 	const argv = getArgv(process.argv);
 	console.log(argv);
-	printError();
 
 	if (argv.h) {
 		printHelp();
@@ -15,6 +27,7 @@ const startCLI = () => {
 		// save city
 	}
 	if (argv.t) {
+		return saveToken(argv.t);
 		// token
 	}
 	// result
